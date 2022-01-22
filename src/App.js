5fd54b1da3d1381c061fdc7c172093c9492a44ee
Har <haroldocampo@live.com>
2022-01-22 12:27:58 +0800

    start game loop

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   SPEED,
   DIRECTIONS
 } from './gameSettings'
+import { useInterval } from './useInterval'
 import { useState, useEffect, useRef } from 'react'
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
   const [snake, setSnake] = useState(SNAKE_START)
   const [apple, setApple] = useState(APPLE_START)
   const [dir, setDir] = useState([0,-1])
-  const [speed, setSpeed] = useState(null)
+  const [speed, setSpeed] = useState(500)
   const [gameOver, setGameOver] = useState(false)
 
   const startGame = () => {
@@ -41,7 +42,13 @@ function App() {
   }
 
   const gameLoop = () => {
+    const tmpSnake = JSON.parse(JSON.stringify(snake));
+    const newSnakeHead = [tmpSnake[0][0] + dir[0], tmpSnake[0][1] + dir[1]];
 
+    tmpSnake.unshift(newSnakeHead); // move snake head 1 pixel to target direction
+    tmpSnake.pop(); // remove last node of snake
+
+    setSnake(tmpSnake)
   }
 
   useEffect(() => {
@@ -56,8 +63,10 @@ function App() {
     context.fillRect(apple[0], apple[1], 1, 1)
   }, [snake, apple, gameOver])
 
+  useInterval(() => gameLoop(), speed)
+
   return (
-    <div role='button' tabIndex='0' keyDown={(e) => moveSnake(e)}>
+    <div role='button' tabIndex='0' onKeyDown={(e) => moveSnake(e)}>
       <canvas
         style={{ border: 'solid 1px #000' }}
         ref={canvasRef}
